da6bf7911f15ebef6abd449c193cb5e8f7a5e953
cynthia <calvocyn@gmail.com>
2020-02-20 09:49:28 -0600

    randomly generated pokemon of each type, no repeats

--- simulation/src/simulation/Simulation_Background.java
+++ simulation/src/simulation/Simulation_Background.java
@@ -3,28 +3,51 @@ package simulation;
 import java.util.Random;
 
 public class Simulation_Background {
-	private String pokemonNameBank[][];
-	/**the above line is where we will store the types of pokemon 
-	 * we have chosen previously to randomly pick from
-	 */
+	private String pokemonNameBank[][] = {
+			{"f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9"},
+			{"g0", "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9"},
+			{"w0", "w1", "w2", "w3", "w4", "w5", "w6", "w7", "w8", "w9"}
+	};
+	private boolean noRepeats[][];
 	private String pokemonNames[][];
-	private int pokemonInt[][];
-	private int time;
 	
-	public Simulation_Background(int length) {
+	public Simulation_Background() {
 		pokemonNames = new String[3][10];
-		pokemonInt = new int[3][10];
+		noRepeats = new boolean[3][10];
 		/**will set the extra spots at the end(if there are any) to 
-		 * something that the program to pick up so that it doesn't count them
+		 * "null" so that it doesn't count them
 		 */
-		time = 0;
 	}
-	
-	private void pickPokemonRandom(int length) {
+	/**
+	 * int level is the type it will randomly be choosing from
+	 * int length is the number of pokemon they wanted from that type
+	 * @param length
+	 * @param level
+	 */
+	public void pickPokemonRandom(int length, int level) {
 		Random random = new Random();
-		int rando = random.nextInt(length);
 		int x = 0;
 		while(x < length) {
+			int rando = random.nextInt(10);
+			if(pokemonNames[level][x].equals("null") && noRepeats[level][rando] != true) {
+				pokemonNames[level][x] = pokemonNameBank[level][rando];
+				System.out.println(pokemonNames[level][x]);
+				noRepeats[level][rando] = true;
+				x++;
+			}
+		}
+		System.out.println("\n");
+	}
+	
+	/**
+	 * will set all places to "null", and the ones not set as pokemon later will stay as "null"
+	 * so the program will be able to tell how many pokemon of each type there are
+	 */
+	public void resetPokemonNames() {
+		for(int x = 0; x < 3; x++) {
+			for(int y = 0; y < 10; y++) {
+				pokemonNames[x][y] = "null";
+			}
 		}
 	}
 }
--- simulation/src/simulation/Simulation_Main.java
+++ simulation/src/simulation/Simulation_Main.java
@@ -1,10 +1,29 @@
 package simulation;
 
+import java.util.Scanner;
+
 public class Simulation_Main {
 
 	public static void main(String[] args) {
-		Simulation_Game test = new Simulation_Game();
-
+		Simulation_Background back = new Simulation_Background();
+		//Simulation_Game test = new Simulation_Game();
+		Scanner input = new Scanner(System.in);
+		
+		int[] level = new int[3];
+		//i'm doing input here just to make sure everything works
+		System.out.println("How many fire?:");
+		level[0] = input.nextInt();
+		System.out.println("How many grass?:");
+		level[1] = input.nextInt();
+		System.out.println("How many water?:");
+		level[2] = input.nextInt();
+		
+		back.resetPokemonNames();
+		
+		for(int i = 0; i < 3; i++) {
+			if(level[i] != 0) {
+				back.pickPokemonRandom(level[i], i);
+			}
+		}
 	}
-
 }
