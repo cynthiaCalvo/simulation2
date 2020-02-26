db3862454098c14bcb8df7a79a8962c9f24e0930
David <Davidsawatzky2002@gmail.com>
2020-02-26 11:38:56 -0600

    Finally got github working again

--- simulation/src/simulation/Battle.java
+++ simulation/src/simulation/Battle.java
@@ -1,4 +1,4 @@
-package simulation;
+package simulation; 
 
 import java.util.Random;
 
@@ -13,11 +13,40 @@ public class Battle {
 	/*
 	 * Randomly selects the pokemon to be used in battle
 	 */
-	public int pokemonSelector() {
+	// public int pokemonSelector() {
+	//	Random r = new Random();
+	//	int pokemon, type;
+		
+	//	pokemon = r.nextInt(10); //selects a random number between 0-9 to return to the main
+		
+	//	type = r.nextInt(3); //selects a random number between 0-2 to return to the main branch to 
+		
+	//	return 0; //placeholder rn
+	//}
+	/*
+	 * I made all of this a comment because I think you've already made a program for this
+	 */
+	
+	/*
+	 * Chooses a move for the pokemon to use based on it's type.
+	 * 0 is fire
+	 * 1 is grass
+	 * 2 is water
+	 */
+	public String pickAttack(int type) {
 		Random r = new Random();
-		int pokemon1, pokemon2; //hiw
+		int randomAttack = 0;
+		String attack = "Ain't working chief";
 		
+		type = 0; //not permanent, this is for testing
 		
-		return 0; //placeholder rn
+		switch(type) {
+		case 0: randomAttack = r.nextInt(4); System.out.println(randomAttack); attack = "hi";break; //fire attacks (hi is a placeholder to see if things work)
+		case 1: randomAttack = r.nextInt(4); //grass attacks
+		case 2: randomAttack = r.nextInt(4); //water attacks
+		default: System.out.println("Error: Type not specified in Battle.java");
+		}
+		
+		return attack; //placeholder
 	}
-} //test 2
+}
